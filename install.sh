aa009861edbbd8ca23d15b34ab6c849c159b37e4
BerenMillidge <bmillidgework@gmail.com>
2021-08-25 21:40:38 +0100

    still fighting the bash script

--- install.sh
+++ install.sh
@@ -6,23 +6,22 @@
 
 echo "INSTALLING DOCKER"
 apt-get update
+apt-get install -y sudo
 apt-get remove -y docker docker-engine docker.io
-apt-get install -y —no-install-recommends \  
-    apt-transport-https \  
-    ca-certificates \  
-    curl
-apt-get install software-properties-common
+apt-get install -y apt-transport-https
+apt-get install -y ca-certificates
+apt-get install -y curl
+apt-get install -y software-properties-common
 apt-get install -y apt-transport-https ca-certificates gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
- echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+echo \
+"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
 # make
 apt-get -y install build-essential
 apt-get -y install make
-apt-get install -y sudo
 
 # add nonroot user
 sudo adduser site
