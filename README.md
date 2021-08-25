aaa88d1ecb22ca65db9a574497b34d29a8322ac3
BerenMillidge <bmillidgework@gmail.com>
2021-08-25 21:31:49 +0100

    updating docker install

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # blog_deployment
 deployment files for my personal blog 
+
+To start from scratch just ssh into your server and then run
+```
+apt update
+apt upgrade -y
+apt install -y git
+git clone 
+https://github.com/BerenMillidge/blog_deployment
+
+cd blog_deployment
+bash install.sh
--- install.sh
+++ install.sh
@@ -3,16 +3,21 @@
 # installation file to get blog up and running on home server
 
 # install basic dependencies
+
+echo "INSTALLING DOCKER"
 apt-get remove -y docker docker-engine docker.io
 apt-get install -y —no-install-recommends \  
     apt-transport-https \  
     ca-certificates \  
     curl \  
     software-properties-common
-curl -fsSL [https://download.docker.com/linux/ubuntu/gpg](https://download.docker.com/linux/ubuntu/gpg) | apt-key add —
-add-apt-repository "deb \[arch=amd64\] [https://download.docker.com/linux/ubuntu](https://download.docker.com/linux/ubuntu) $(lsb_release -cs) stable"
-apt-get updateapt-get install -y docker-ceusermod -aG docker $(id -u -n)
-
+apt-get install -y apt-transport-https ca-certificates gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io
 # make
 apt-get -y install build-essential
 apt-get -y install make
@@ -41,3 +46,6 @@ docker-compose up -d
 
 # pull down the github repo (requires you to enter your credentials)
 git clone https://github.com/BerenMillidge/BerenMillidge.github.io
+
+# then update to run
+bash ./update_site.sh
